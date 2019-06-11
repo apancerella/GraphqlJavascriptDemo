222cc1faee5dc3220787d9dca1daa9dd5fbeca80
Anthony Pancerella <apancerella@definitivelogic.com>
2019-06-10 23:07:40 -0400

    updated with graph tools

--- resolvers.js
+++ resolvers.js
@@ -3,36 +3,41 @@ const friendDatabase = {};
 
 
 class Friend {
-    constructor(id, {firstName, lastName, gender, language, email }) {
+    constructor(id, {firstName, lastName, gender, age, language, email, contacts }) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
+        this.age = age;
         this.language = language;
         this.email = email;
+        this.contacts = contacts
     }
 }
 
-const resolvers = {
-    friend: () => {
-        return {
-            "id": 123123,
-            "firstName": "Anthony",
-            "lastName": "Pancerella",
-            "gender": "Male",
-            "language": "English",
-            "email": "test@test.com"
-            //"emails": [{ email: "test@test.com" }, { email: "test2@test.com" }]
+//resolver map
+export const resolvers = {
+    Query: {
+        friend: () => {
+            return {
+                "id": 123123,
+                "firstName": "Anthony",
+                "lastName": "Pancerella",
+                "gender": "Male",
+                "age": "24",
+                "language": "English",
+                "email": "test@test.com"
+            }
+        },    
+        getFriend: ({ id }) => {
+            return new Friend(id, friendDatabase[id]);
+        },
+    },
+    Mutation: {
+        createFriend: ({input}) => {
+            let id = require("crypto").randomBytes(10).toString('hex');
+            friendDatabase[id] = input;
+            return new Friend(id, input);
         }
-    },
-    getFriend: ({ id }) => {
-        return new Friend(id, friendDatabase[id]);
-    },
-    createFriend: ({input}) => {
-        let id = require("crypto").randomBytes(10).toString('hex');
-        friendDatabase[id] = input;
-        return new Friend(id, input);
     }
-};
-
-export default resolvers;
+}
--- schema.js
+++ schema.js
@@ -1,15 +1,26 @@
-import { buildSchema } from 'graphql';
+import { resolvers } from './resolvers';
+import { makeExecutableSchema } from 'graphql-tools';
 
-const schema = buildSchema(`
+const typeDefs = `
     type Friend {
         id: ID
         firstName: String
         lastName: String
-        gender: String
+        gender: Gender
+        age: Int
         language: String
         email: String
+        contacts: [Contact]
     }
-
+    type Contact {
+        firstName: String
+        lastName: String
+    }
+    enum Gender {
+        MALE
+        FEMALE
+        OTHER
+    }
     type Query {
         friend: Friend
         getFriend(id: ID): Friend
@@ -19,14 +30,23 @@ const schema = buildSchema(`
         id: ID
         firstName: String!
         lastName: String
-        gender: String
+        gender: Gender
+        age: Int
         language: String
         email: String
+        contacts: [ContactInput]
+    }
+    
+    input ContactInput {
+        firstName: String
+        lastName: String
     }
 
     type Mutation {
         createFriend(input: FriendInput): Friend
     }
-`)
+`;
 
-export default schema;
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export { schema };
